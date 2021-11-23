8a5327ae3b48eafa8c8244c88167535f7dca7565
unknown <fvarone@fi.uba.ar>
2021-11-23 18:32:39 -0300

    Errores arreglados y modularización

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,4 +1,6 @@
 const { Router, application } = require('express');
+const Profile = require("../models/Profiles")
+const Institution = require("../models/Institution")
 
 const axios = require('axios')
 // Importar todos los routers;
@@ -6,5 +8,40 @@ const axios = require('axios')
 
 
 const router = Router();
+
+router.get('/risa', async (req, res) => {
+
+    var usuario = await Profile.find()
+    res.send("Otro cambio de prueba de Lauuu ")
+})
+
+
+router.get('/prueba', async (req, res) => {
+
+    var usuario = await Profile.find()
+    res.send(usuario)
+})
+
+router.post("/Profile", async (req, res) => {
+    var newProfile = await new Profile({
+        name: req.body.name,
+        email: req.body.email,
+        country: req.body.country,
+        institution: req.body.institution,
+
+
+    })
+    newProfile.save();
+    res.send(newProfile)
+})
+
+router.post("/Institution", async (req, res) => {
+    var newInstitution = await new Institution({
+        name: req.body.name
+    })
+    newInstitution.save();
+    res.send(newInstitution)
+})
+
 
 module.exports = router;
