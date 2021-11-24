49a3e3d67ba6a7be3a9cb4d3172a25ac3dc9cd07
SebastianAnea <sebastiananea@gmail.com>
2021-11-24 15:03:59 -0300

    script nodemon, componente Meet

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,20 @@
-import React from 'react';
-import { BrowserRouter, Route} from "react-router-dom";
-import {LandingPage, Register, NavBar} from "./components/index";
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+import { LandingPage, Register, NavBar } from './components/index'
+import './App.css'
+import Meet from './components/Meet/Meet'
 
-const App =()=> {
+const App = () => {
   return (
     <BrowserRouter>
-      <div className="App">
-        <Route path="/" component={NavBar}/>
-        <Route path="/login" component={LandingPage}/>
-        <Route path="/signup" component={Register}/>
+      <div className='App'>
+        <Route path='/' component={NavBar} />
+        <Route path='/login' component={LandingPage} />
+        <Route path='/signup' component={Register} />
+        <Route path='/meet' component={Meet} />
       </div>
     </BrowserRouter>
-  );
-};
+  )
+}
 
-export default App;
+export default App
