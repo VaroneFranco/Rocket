53d9529b75bb7089362ab44c8fcc57141d561435
Cristian Nicolás Castagnetto <cncastagnetto@hotmail.com>
2021-11-25 14:18:20 -0300

    componente Loading

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import style from "./Home.module.css";
 import Silla from '../Silla/Silla.jsx';
+import Loading from '../Loading/Loading.jsx';
 
 const arr = [
-    {name: "josé",surname: "Perez",img: "https://ath2.unileverservices.com/wp-content/uploads/sites/5/2018/05/peinado-hacia-atraxxs-hombre4-300x300.jpg"},
-    {name: "Alberto",surname: "Gonzalez",img: "https://ath2.unileverservices.com/wp-content/uploads/sites/5/2018/05/peinado-hacia-atraxxs-hombre4-300x300.jpg"},
-    {name: "Jorge",surname: "Martinez",img: "https://ath2.unileverservices.com/wp-content/uploads/sites/5/2018/05/peinado-hacia-atraxxs-hombre4-300x300.jpg"},
-    {name: "Carlos",surname: "Costa",img: "https://ath2.unileverservices.com/wp-content/uploads/sites/5/2018/05/peinado-hacia-atraxxs-hombre4-300x300.jpg"}
+    // {name: "José",surname: "Perez",img: "https://ath2.unileverservices.com/wp-content/uploads/sites/5/2018/05/peinado-hacia-atraxxs-hombre4-300x300.jpg"},
+    // {name: "Alberto",surname: "Gonzalez",img: "https://ath2.unileverservices.com/wp-content/uploads/sites/5/2018/05/peinado-hacia-atraxxs-hombre4-300x300.jpg"},
+    // {name: "Jorge",surname: "Martinez",img: "https://ath2.unileverservices.com/wp-content/uploads/sites/5/2018/05/peinado-hacia-atraxxs-hombre4-300x300.jpg"},
+    // {name: "Carlos",surname: "Costa",img: "https://ath2.unileverservices.com/wp-content/uploads/sites/5/2018/05/peinado-hacia-atraxxs-hombre4-300x300.jpg"}
 ];
 
 const Home =()=> {
@@ -18,13 +19,17 @@ const Home =()=> {
                 </div>
                 <div className={style.home__mesa__child}>
                    {
-                       arr.length && arr.map(user => (
+                       arr.length 
+                       ? 
+                       arr.map(user => (
                            <Silla
                            name = {user.name}
                            surname = {user.surname}
                            img = {user.img}
                            />
                        ))
+                       :
+                       <Loading />
                    }
                 </div>
             </div>
