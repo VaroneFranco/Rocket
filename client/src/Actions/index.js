9c94917b8ecb1dde4f4e5f284d1443cd48daef40
unknown <fvarone@fi.uba.ar>
2021-11-25 16:43:58 -0300

    Template reducer y cambio

---
+++ client/src/Actions/index.js
@@ -0,0 +1,9 @@
+const USER_CHANGES = "USER_CHANGES";
+const GET_PROFILES = "GET_PROFILES";
+const IS_LOG = "IS_LOG";
+const ASIGN_TABLE = "ASIGN_TABLE";
+const SEARCH_USER = "SEARCH_USER";
+const SEARCH_USER_ID="SEARCH_USER_ID";
+
+import axios from "axios";
+
