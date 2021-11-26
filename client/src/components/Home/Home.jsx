8aed704f2d7e4f1293712ed71c2b4a33b30574c7
SebastianAnea <sebastiananea@gmail.com>
2021-11-26 14:04:57 -0300

    user LocalStorage andando anasheee

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,43 +1,57 @@
-import React from 'react';
-import style from "./Home.module.css";
-import Silla from '../Silla/Silla.jsx';
-import Loading from '../Loading/Loading.jsx';
+import React, { useEffect } from 'react'
+import style from './Home.module.css'
+import Silla from '../Silla/Silla.jsx'
+import Loading from '../Loading/Loading.jsx'
 
 const arr = [
-    {name: "José",surname: "Perez",img: "https://ath2.unileverservices.com/wp-content/uploads/sites/5/2018/05/peinado-hacia-atraxxs-hombre4-300x300.jpg"},
-    {name: "Alberto",surname: "Gonzalez",img: "https://ath2.unileverservices.com/wp-content/uploads/sites/5/2018/05/peinado-hacia-atraxxs-hombre4-300x300.jpg"},
-    {name: "Jorge",surname: "Martinez",img: "https://ath2.unileverservices.com/wp-content/uploads/sites/5/2018/05/peinado-hacia-atraxxs-hombre4-300x300.jpg"},
-    {name: "Carlos",surname: "Costa",img: "https://ath2.unileverservices.com/wp-content/uploads/sites/5/2018/05/peinado-hacia-atraxxs-hombre4-300x300.jpg"}
-];
+  {
+    name: 'José',
+    surname: 'Perez',
+    img: 'https://ath2.unileverservices.com/wp-content/uploads/sites/5/2018/05/peinado-hacia-atraxxs-hombre4-300x300.jpg',
+  },
+  {
+    name: 'Alberto',
+    surname: 'Gonzalez',
+    img: 'https://ath2.unileverservices.com/wp-content/uploads/sites/5/2018/05/peinado-hacia-atraxxs-hombre4-300x300.jpg',
+  },
+  {
+    name: 'Jorge',
+    surname: 'Martinez',
+    img: 'https://ath2.unileverservices.com/wp-content/uploads/sites/5/2018/05/peinado-hacia-atraxxs-hombre4-300x300.jpg',
+  },
+  {
+    name: 'Carlos',
+    surname: 'Costa',
+    img: 'https://ath2.unileverservices.com/wp-content/uploads/sites/5/2018/05/peinado-hacia-atraxxs-hombre4-300x300.jpg',
+  },
+]
 
-const Home =()=> {
-    return (
-        <div className={style.home__container}>
-            <div className={style.home__mesa}>
-                <div>
-                    <h2>My Team</h2>
-                </div>
-                <div className={style.home__mesa__child}>
-                   {
-                       arr.length 
-                       ? 
-                       arr.map(user => (
-                           <Silla
-                           name = {user.name}
-                           surname = {user.surname}
-                           img = {user.img}
-                           />
-                       ))
-                       :
-                       <Loading />
-                   }
-                </div>
-            </div>
-            <div className={style.home__chat}>
-                <h4>CHAT</h4>
-            </div>
+const Home = () => {
+  useEffect(() => {
+    console.log(JSON.parse(localStorage.getItem('user')))
+  }, [])
+
+  return (
+    <div className={style.home__container}>
+      <div className={style.home__mesa}>
+        <div>
+          <h2>My Team</h2>
         </div>
-    );
-};
+        <div className={style.home__mesa__child}>
+          {arr.length ? (
+            arr.map((user) => (
+              <Silla name={user.name} surname={user.surname} img={user.img} />
+            ))
+          ) : (
+            <Loading />
+          )}
+        </div>
+      </div>
+      <div className={style.home__chat}>
+        <h4>CHAT</h4>
+      </div>
+    </div>
+  )
+}
 
-export default Home;
+export default Home
--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -1,108 +1,114 @@
-import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
-import "./LandingPage.css";
-import axios from "axios";
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import './LandingPage.css'
+import axios from 'axios'
 
 function LandingPage() {
-  let history = useHistory();
+  let history = useHistory()
   var [log, setLog] = useState({
-    email: "",
-    password: "",
-  });
+    email: '',
+    password: '',
+  })
   function handleChange(e) {
-    const value = e.target.value;
+    const value = e.target.value
     setLog({
       ...log,
       [e.target.name]: value,
-    });
+    })
   }
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    axios("http://localhost:3001/signin", {
-      method: "post",
+  async function handleSubmit(e) {
+    e.preventDefault()
+    await axios('http://localhost:3001/signin', {
+      method: 'post',
       data: log,
     }).then((r) => {
       if (r.data.token) {
-        console.log("login token: ", r.data.token);
-        localStorage.setItem("token", r.data.token);
-        return history.push("/");
+        console.log('login token: ', r.data.token)
+        localStorage.setItem('token', r.data.token)
+        return history.push('/')
       } else {
         setLog({
-          username: "",
-          password: "",
-        });
-        alert("User or Password incorrect");
+          username: '',
+          password: '',
+        })
+        alert('User or Password incorrect')
       }
-    });
+    })
+    await axios('http://localhost:3001/isLog', {
+      method: 'post',
+      data: { token: localStorage.getItem('token') },
+    })
+      .then((res) => localStorage.setItem('user', JSON.stringify(res.data)))
+      .then(() => history.push('/home'))
   }
 
   return (
-    <div className="container">
-      <div className="create-container">
-        <div className="signIn">
+    <div className='container'>
+      <div className='create-container'>
+        <div className='signIn'>
           <h2>Sign In</h2>
         </div>
-        <div className="create-container-child">
-          <div className="form">
-            <form className="form-child" onSubmit={handleSubmit}>
+        <div className='create-container-child'>
+          <div className='form'>
+            <form className='form-child' onSubmit={handleSubmit}>
               <div>
-                <div className="form-group">
+                <div className='form-group'>
                   <label>
                     <h4>Email</h4>
                   </label>
                 </div>
                 <input
-                  className="landingPage__input"
-                  type="email"
-                  name="email"
+                  className='landingPage__input'
+                  type='email'
+                  name='email'
                   value={log.email}
                   onChange={(e) => handleChange(e)}
                   required
-                  autoComplete="off"
+                  autoComplete='off'
                 />
-                <div className="form-group">
+                <div className='form-group'>
                   <label>
                     <h4>Password</h4>
                   </label>
                 </div>
                 <input
-                  className="landingPage__input"
-                  type="password"
-                  name="password"
+                  className='landingPage__input'
+                  type='password'
+                  name='password'
                   value={log.password}
                   onChange={(e) => handleChange(e)}
                   required
-                  autoComplete="off"
+                  autoComplete='off'
                 />
               </div>
-              <div className="landingPage__button">
-                <button className="landingPage__button_login" type="submit">
-                  <h4 className="landingPage__button_text">LOG IN</h4>
+              <div className='landingPage__button'>
+                <button className='landingPage__button_login' type='submit'>
+                  <h4 className='landingPage__button_text'>LOG IN</h4>
                 </button>
               </div>
             </form>
           </div>
-          <div className="landingPage__login_image"></div>
+          <div className='landingPage__login_image'></div>
           <h5>or login with</h5>
-          <div className="landingPage__image">
+          <div className='landingPage__image'>
             <img
-              src="https://www.lasbrisashotels.com.mx/wp-content/uploads/2021/08/pngwing.com_.png"
-              alt="Google"
-              width="40px"
-              height="40px"
+              src='https://www.lasbrisashotels.com.mx/wp-content/uploads/2021/08/pngwing.com_.png'
+              alt='Google'
+              width='40px'
+              height='40px'
             />
             <img
-              src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
-              alt="Github"
-              width="40px"
-              height="40px"
+              src='https://cdn-icons-png.flaticon.com/512/25/25231.png'
+              alt='Github'
+              width='40px'
+              height='40px'
             />
           </div>
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default LandingPage;
+export default LandingPage
