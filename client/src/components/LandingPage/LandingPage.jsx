42a71375b87ac9bad069ec10d2ad31e3daacff91
LiamMarega <liammarega85@gmail.com>
2021-11-26 16:20:56 -0300

    Arreglo de botones  Login

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -57,82 +57,81 @@ function LandingPage() {
   };
 
   return (
-    <div className='container'>
-      <div className='create-container'>
-        <div className='signIn'>
+    <div className="container">
+      <div className="create-container">
+        <div className="signIn">
           <h2>Sign In</h2>
         </div>
-        <div className='create-container-child'>
-          <div className='form'>
-            <form className='form-child' onSubmit={handleSubmit}>
+        <div className="create-container-child">
+          <div className="form">
+            <form className="form-child" onSubmit={handleSubmit}>
               <div>
-                <div className='form-group'>
+                <div className="form-group">
                   <label>
                     <h4>Email</h4>
                   </label>
                 </div>
                 <input
-                  className='landingPage__input'
-                  type='email'
-                  name='email'
+                  className="landingPage__input"
+                  type="email"
+                  name="email"
                   value={log.email}
                   onChange={(e) => handleChange(e)}
                   required
-                  autoComplete='off'
+                  autoComplete="off"
                 />
-                <div className='form-group'>
+                <div className="form-group">
                   <label>
                     <h4>Password</h4>
                   </label>
                 </div>
                 <input
-                  className='landingPage__input'
-                  type='password'
-                  name='password'
+                  className="landingPage__input"
+                  type="password"
+                  name="password"
                   value={log.password}
                   onChange={(e) => handleChange(e)}
                   required
-                  autoComplete='off'
+                  autoComplete="off"
                 />
               </div>
-              <div className='landingPage__button'>
-                <button className='landingPage__button_login' type='submit'>
-                  <h4 className='landingPage__button_text'>LOG IN</h4>
+              <div className="landingPage__button">
+                <button className="landingPage__button_login" type="submit">
+                  <h4 className="landingPage__button_text">LOG IN</h4>
                 </button>
               </div>
             </form>
           </div>
-          <div className='landingPage__login_image'></div>
+          <div className="landingPage__login_image"></div>
           <h5>or login with</h5>
 
           <div className="landingPage__image">
             <button onClick={() => handleOnClick(facebookProvider)}>
-              {" "}
-              Facebook{" "}
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg"
+                alt="Facebook"
+                
+              />
             </button>
             <button onClick={() => handleOnClick(githubProvider)}>
-              {" "}
               <img
                 src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
                 alt="Github"
-                width="40px"
-                height="40px"
-              />{" "}
+                
+              />
             </button>
             <button onClick={() => handleOnClick(googleProvider)}>
               <img
                 src="https://www.lasbrisashotels.com.mx/wp-content/uploads/2021/08/pngwing.com_.png"
                 alt="Google"
-                width="40px"
-                height="40px"
+             
               />
             </button>
-
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default LandingPage
