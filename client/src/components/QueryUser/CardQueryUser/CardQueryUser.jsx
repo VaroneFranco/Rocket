bebe7c5e7160794701fd7ff7dafae7c94eaf59ab
LiamMarega <liammarega85@gmail.com>
2021-11-27 19:38:57 -0300

    Avance QueryUser

--- client/src/components/QueryUser/CardQueryUser/CardQueryUser.jsx
+++ client/src/components/QueryUser/CardQueryUser/CardQueryUser.jsx
@@ -1,41 +1,36 @@
 import React from 'react'
 import s from "./CardQueryUser.module.css";
-
-function CardQueryUser(
-  img,
-  name,
-  about,
-  country,
-  institution,
-  score,
-  absence,
-  reports
-) {
-  return (
-    <>
-      <div className={s.containerQuery}>
-        <div className={s.containerFoto}>
-          <div className={s.foto}>
-            <img
-              src="https://concepto.de/wp-content/uploads/2018/08/persona-e1533759204552.jpg"
-              alt="foto de perfil"
-            />
+import Loading from "../../Loading/Loading";
+function CardQueryUser({user}) {
+ if (user) {
+    return (
+      <>
+        <div className={s.containerQuery}>
+          <div className={s.containerFoto}>
+            <div className={s.foto}>
+              <img
+                src="https://concepto.de/wp-content/uploads/2018/08/persona-e1533759204552.jpg"
+                alt="foto de perfil"
+              />
+            </div>
+            <div className={s.foto_nombre}>{user.name}</div>
+            <div className={s.foto_about}>about</div>
           </div>
-          <div className={s.foto_nombre}>{name}</div>
-          <div className={s.foto_about}>about</div>
-        </div>
-        <div className={s.containerInfo}>
-          <div className={s.info_nombre}>
+          <div className={s.containerInfo}>
+            <div className={s.info_nombre}>
               NOMBRE APELLIDO UBICACION ADMIN/ESTUDENT
-              </div>
-              <div className={s.info_nombre}>
+            </div>
+            <div className={s.info_nombre}>
               NOMBRE APELLIDO UBICACION ADMIN/ESTUDENT
-              </div>
-        
+            </div>
+          </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+ }
+ return (
+  <Loading />
+ )
 }
 
 export default CardQueryUser
--- client/src/components/QueryUser/QueryUser.jsx
+++ client/src/components/QueryUser/QueryUser.jsx
@@ -1,26 +1,28 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import style from "./QueryUser.module.css";
 import { Link, useParams } from "react-router-dom";
 import axios from "axios";
-import Loading from "../Loading/Loading.jsx";
 import s from "./QueryUser.module.css"
 import CardQueryUser from "./CardQueryUser/CardQueryUser";
 
 function QueryUser() {
 
   const { _id } = useParams();
+  console.log(_id);
+  var [user, setUser] = useState({});
 
-  useEffect ( async () => {
-    const user = await axios(`http://localhost:3001/searchProfileID/${_id}`);
+  useEffect (  () => {
+        axios(`http://localhost:3001/searchProfileID/${_id}`).then(res => setUser(res.data));
 
-    console.log(user.data);
+    console.log(user);
   }, [] );
 
   return (
+
     <div className={s.QueryUser}>
+
       <div className={s.mainContainer}>
-        <CardQueryUser
-        />
+       <CardQueryUser user={user} /> 
       </div>
     </div>
   );
