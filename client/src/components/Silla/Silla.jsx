6813d7649c942118a9b6036f7e12352535f7573d
Guillermo L. Martos <guille.l.martos@gmail.com>
2021-11-27 17:49:33 -0300

    cambio meta del head de client/public/index para que diga algo más lindo que App hecha con React... y hago disable Like y Report para que no puedas apretar mil veces, solo una

--- client/src/components/Silla/Silla.jsx
+++ client/src/components/Silla/Silla.jsx
@@ -1,17 +1,27 @@
-import React from "react";
+import {useState} from "react";
 import style from "./Silla.module.css";
 import axios from "axios";
 
 function Silla({ img, name, surname, _id }) {
+  
+  //variables que sirven para deshabilitar botones like/report cuando se presionan
+  const [likeOrReport, setlikeOrReport] = useState({like:false, report:false})
+
+    
   const onChange = async (e) => {
     e.preventDefault();
     if (e.target.value === "like") {
       axios(`localhost:3001/increaseLike/${_id}`);
+      setlikeOrReport({...likeOrReport, like:true})
     }
     if (e.target.value === "reports"){
         axios(`localhost:3001/increaseReports/${_id}`)
+        setlikeOrReport({...likeOrReport, report:true})
     }
   };
+
+
+
 
   return (
     <div className={style.silla__container}>
@@ -21,11 +31,21 @@ function Silla({ img, name, surname, _id }) {
       <h4 className={style.silla__surname}>{surname}</h4>
 
       <select className={style.silla__select} onChange={(e) => onChange(e)}>
-        <option value="">Like / Report</option>
+        <option disabled selected value="">Like / Report</option>
+        {likeOrReport.like===false ? 
         <option value="like" name="">
           Like 👍
-        </option>
-        <option value="reports">Report 🚫</option>
+        </option> :
+         <option disabled value="like" name="">
+         Like 👍
+       </option>
+        }
+        {likeOrReport.report===false ? 
+        <option value="reports">Report 🚫</option> :
+        <option disabled value="reports">Report 🚫</option>
+        }
+        
+        
       </select>
     </div>
   );
