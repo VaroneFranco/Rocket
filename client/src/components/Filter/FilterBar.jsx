8b24419bccea3971a0eb85dce48ccb3127ae13de
Ignacio Aranda <87491116+elnacho02@users.noreply.github.com>
2021-11-28 19:12:01 -0300

    filterBar a landing page

---
+++ client/src/components/Filter/FilterBar.jsx
@@ -0,0 +1,44 @@
+import React,{ useState } from 'react'
+import { useHistory, useLocation } from "react-router-dom"
+import s from "./FilterBar.module.css"
+
+
+
+function FilterBar({Types, setOrder}) {
+    
+    let history = useHistory()
+    var [orderBy, setOrderBy] = useState("default")
+    function handleOrder(e){
+        setOrder(e.target.value)
+        setOrderBy(e.target.value)
+    }
+    
+    return(
+        <div className={s.container}>
+            <input type="checkbox" className={s.checkbox} id='check'/>
+            <label for="check" className={s.menu}>
+                <img src="http://cdn.onlinewebfonts.com/svg/img_211250.png" alt="" />
+            </label>
+            <div className={s.topPanel}>
+                <div className={s.orderByContainer}>
+                        <div className={s.orderBy}>
+                        <span>Order By</span>
+                            <select value={orderBy} className={s.order} onChange={handleOrder}>
+                                <option value="default">Default</option>
+                                <option value="a-z">A-Z</option>
+                                <option value="z-a">Z-A</option>
+                                <option value="higher-rockets">Higher Rockets</option>
+                            </select>  
+                        </div>
+                    </div>
+            </div>
+
+        </div>
+
+
+    )
+}
+
+
+export default FilterBar
+
---
+++ client/src/components/utils.js
@@ -0,0 +1,13 @@
+ 
+
+
+function ordenar(array,key){
+    
+    if(key === "a-z") return array.sort((a,b)=> (a.name.toLowerCase() > b.name.toLowerCase()) ? 1:-1)
+    if(key === "z-a") return array.sort((a,b)=> (a.name.toLowerCase() < b.name.toLowerCase()) ? 1:-1)
+    if(key === "higher-rockets") return array.sort((a,b)=> (a.score < b.score) ? 1:-1)
+}
+
+module.exports = {
+    ordenar
+}
