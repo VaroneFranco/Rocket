b845190f1f931bfa463f7d7599fa7918b0441677
Ignacio Aranda <87491116+elnacho02@users.noreply.github.com>
2021-11-28 22:45:01 -0300

    log fixed

--- client/src/components/TrueHome/TrueHome.jsx
+++ client/src/components/TrueHome/TrueHome.jsx
@@ -11,26 +11,40 @@ function TrueHome() {
   let history = useHistory()
   var [pag, setPag] = useState(0)
   var [users, setUsers] = useState([])
-
+  var myUserVar = JSON.parse(localStorage.getItem('user'))
   useEffect(async () => {
-    var myUser = JSON.parse(localStorage.getItem('user'))
-
-    await axios('http://localhost:3001/getUsersByInstitution', {
+    /* async function getUsers (){
+        var myUser = JSON.parse(localStorage.getItem('user'))
+        await axios('http://localhost:3001/getUsersByInstitution', {
+        method: 'post',
+        data: {
+        institution: myUser.institution,
+      },
+    }).then((x) => setUsers(x.data.filter((x) => x._id !== myUser._id)))
+    }
+    async function asignTable(){
+        await axios.post('http://localhost:3001/asignTable')
+    }
+    getUsers()
+    asignTable() */
+    /* await axios('http://localhost:3001/isLog', {
       method: 'post',
-      data: {
+      data: { token: localStorage.getItem('token') },
+    }).then((res) => localStorage.setItem('user', JSON.stringify(res.data))) */
+    var myUser = JSON.parse(localStorage.getItem('user'))
+        await axios('http://localhost:3001/getUsersByInstitution', {
+        method: 'post',
+        data: {
         institution: myUser.institution,
       },
     }).then((x) => setUsers(x.data.filter((x) => x._id !== myUser._id)))
     await axios.post('http://localhost:3001/asignTable')
-    await axios('http://localhost:3001/isLog', {
-      method: 'post',
-      data: { token: localStorage.getItem('token') },
-    }).then((res) => localStorage.setItem('user', JSON.stringify(res.data)))
   }, [])
 
   async function handleClick(e) {
     history.push('/home')
   }
+ 
 
   return (
     <div className={s.container}>
