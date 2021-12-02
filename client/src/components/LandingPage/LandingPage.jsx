a9cf19201b38b81089d6de328495174fda62032a
LiamMarega <liammarega85@gmail.com>
2021-12-02 14:14:21 -0300

    Cambios minimos

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react'
 import { useHistory } from 'react-router-dom'
+import GitHub from "../../Images/github.png";
+import Facebook from "../../Images/Facebook.png";
+import Google from "../../Images/google-logo-9808.png";
 import './LandingPage.css'
 import axios from 'axios'
 
@@ -76,78 +79,72 @@ function LandingPage() {
   }
  
   return (
-    <div className='container'>
-      <div className='create-container'>
-        <div className='signIn'>
+    <div className="container">
+      <div className="create-container">
+        <div className="signIn">
           <h2>Sign In</h2>
         </div>
-        <div className='create-container-child'>
-          <div className='form'>
-            <form className='form-child' onSubmit={handleSubmit}>
+        <div className="create-container-child">
+          <div className="form">
+            <form className="form-child" onSubmit={handleSubmit}>
               <div>
-                <div className='form-group'>
+                <div className="form-group">
                   <label>
                     <h4>Email</h4>
                   </label>
                 </div>
                 <input
-                  className='landingPage__input'
-                  type='email'
-                  name='email'
+                  className="landingPage__input"
+                  type="email"
+                  name="email"
                   value={log.email}
                   onChange={(e) => handleChange(e)}
                   required
-                  autoComplete='off'
+                  autoComplete="off"
                 />
-                <div className='form-group'>
+                <div className="form-group">
                   <label>
                     <h4>Password</h4>
                   </label>
                 </div>
                 <input
-                  className='landingPage__input'
-                  type='password'
-                  name='password'
+                  className="landingPage__input"
+                  type="password"
+                  name="password"
                   value={log.password}
                   onChange={(e) => handleChange(e)}
                   required
-                  autoComplete='off'
+                  autoComplete="off"
                 />
               </div>
-              <div className='landingPage__button'>
-                <button className='landingPage__button_login' type='submit'>
-                  <h4 className='landingPage__button_text'>LOG IN</h4>
+              <div className="landingPage__button">
+                <button className="landingPage__button_login" type="submit">
+                  <h4 className="landingPage__button_text">LOG IN</h4>
                 </button>
               </div>
             </form>
           </div>
-          <div className='landingPage__login_image'></div>
+          <div className="landingPage__login_image"></div>
           <h5>or login with</h5>
 
-          <div className='landingPage__image'>
+          <div className="landingPage__image">
             <button onClick={() => handleOnClick(facebookProvider)}>
-              <img
-                src='https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg'
-                alt='Facebook'
-              />
+              <img src={Facebook} alt="Facebook" />
             </button>
             <button onClick={() => handleOnClick(githubProvider)}>
-              <img
-                src='https://cdn-icons-png.flaticon.com/512/25/25231.png'
-                alt='Github'
-              />
+              <img src={GitHub} alt="Github" />
             </button>
             <button onClick={() => handleOnClick(googleProvider)}>
               <img
-                src='https://www.lasbrisashotels.com.mx/wp-content/uploads/2021/08/pngwing.com_.png'
-                alt='Google'
+                src={Google}
+                alt="Google"
               />
             </button>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default LandingPage
