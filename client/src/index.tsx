05e2f07bff5ab7e31802905c3de48220ea3a6e91
ÂeLNacho02 <nacho.17012002@gmail.com>
2021-12-04 01:55:00 -0300

    bootstrap import X

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './Store/index';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+/* import 'bootstrap/dist/css/bootstrap.min.css'; */
 
 ReactDOM.render(
   <Provider store={store}>
